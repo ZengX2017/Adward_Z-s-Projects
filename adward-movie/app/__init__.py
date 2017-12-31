becaa32b87de40b8eff4464d700ec3f71c4cfac7
ZengX2017 <adwardzx@outlook.com>
2017-12-31 17:27:13 +0800

    在线电影项目

---
+++ adward-movie/app/__init__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+__author__ = 'Adward_Z'
+
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+import pymysql
+import os
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:zx123456@127.0.0.1:3306/movie"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+app.config["SECRET_KEY"] = "b9da9109cf5347c9b115beada3cf9d3b"
+app.config["UP_DIR"] = os.path.join(os.path.abspath(os.path.dirname(__file__)), "static/uploads/")
+app.config["FC_DIR"] = os.path.join(os.path.abspath(os.path.dirname(__file__)), "static/uploads/users")
+
+app.debug = True
+db = SQLAlchemy(app)
+
+from app.home import home as home_blueprint
+from app.admin import admin as admin_blueprint
+
+app.register_blueprint(home_blueprint)
+app.register_blueprint(admin_blueprint, url_prefix="/admin")
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("home/404.html"), 404
---
+++ adward-movie/manage.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+__author__ = 'Adward_Z'
+
+from app import app
+
+if __name__ == "__main__":
+    app.run()
